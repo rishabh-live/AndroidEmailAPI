f0bed919916eb4d3a1fc87b9b3c4664041de4510
rishabharya1422 <rishabh12536@gmail.com>
2020-03-29 07:18:17 +0530

    First Update

---
+++ Email.java
@@ -0,0 +1,34 @@
+
+public class Email {
+
+    String emailid = "blank";
+    String emailpassword = "blank";
+    String emailmessage = "blank";
+    String toemail = "blank";
+
+    public String EmailId(final String email) {
+
+        emailid = email;
+        return emailid;
+        // return password;
+
+    }
+
+    public String Password(final String password) {
+        emailpassword = password;
+        return emailpassword;
+    }
+
+
+
+    public String SimpleMail(final String message){
+        emailmessage = message;
+        return emailmessage;
+    }
+
+    public String ToEmail(final String to){
+        toemail = to;
+        return toemail;
+    }
+
+}
