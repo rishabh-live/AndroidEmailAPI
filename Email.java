3355dd51f5ae7a2b3705f22bc35179c2966e2138
rishabharya1422 <rishabh12536@gmail.com>
2020-03-29 07:35:49 +0530

    Subject Added. Started Working on execute method

--- Email.java
+++ Email.java
@@ -7,6 +7,7 @@ public class Email {
     String toemail = "blank";
     String htmlMail = "notavailable";
     String sendotp = "no";
+    String subject = "blank"
 
     public String EmailId(final String email) {
 
@@ -41,6 +42,21 @@ public class Email {
     public String SendOtp(String send){
       sendotp = send;
       return sendotp;
+    }
+
+    public String Subject(String subj){
+      Subject = subj;
+      return Subject;
+    }
+
+    public void execute()
+    {
+      if (emailid.equals("blank")) {
+        System.out.println("Error 4041: Email ID not found.");
+      }
+      if (password.equals("blank")) {
+        System.out.println("Error 4042: Password not found.")
+      }
     }
 
 }
